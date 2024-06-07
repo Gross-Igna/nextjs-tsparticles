aa642631503928025f61b32c6cef69d5469af57c
Gross-Igna <grossignacio01@gmail.com>
2024-06-06 23:25:06 -0300

    nextjs with tsparticles

---
+++ src/app/part/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ParticlesBackrgound from '../comp/ParticlesBackrgound'
+const page = () => {
+    return (
+        <ParticlesBackrgound>
+            <h1>Page with particles background !</h1>
+            <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, debitis nostrum est et impedit consequatur id quod autem a omnis unde ipsum, odio sunt nesciunt officia aliquam. Officiis atque ipsa corrupti qui similique distinctio in repellendus sequi libero cum assumenda, earum hic odio, quo laudantium. Nulla doloribus tempore sint amet.
+            </div>
+        </ParticlesBackrgound>
+    )
+}
+
+export default page
